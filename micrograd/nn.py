cab43d531c21692b810b0f251d43df0911e931b2
SouL <watingsoul@gmail.com>
2026-08-24 23:07:23 +0530

    feat: add Neuron Layer and MLP modules

--- micrograd/nn.py
+++ micrograd/nn.py
@@ -1,1 +1,51 @@
 # Neural network building blocks (Module, Neuron, Layer, MLP)
+from micrograd.engine import Value
+import random
+class Module:
+    def parameters(self):
+        return[]
+
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0.0
+
+class Neuron(Module):
+    def __init__(self, nin):
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
+        self.b = Value(0.0)
+
+    def __call__(self, x):
+        act = sum((wi * xi for wi, xi in zip(self.w, x)), Value(0))
+        out = act.tanh()
+        return out
+
+    def parameters(self):
+        return self.w + [self.b]
+
+
+class Layer(Module):
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+
+    def __call__(self, x):
+        outs = [n(x) for n in self.neurons]
+        return outs[0] if len(outs) == 1 else outs
+
+    def parameters(self):
+        return [p for n in self.neurons for p in n.parameters()]
+    
+
+class MLP(Module):
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+    
+
